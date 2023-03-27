d42aa45b6ae0bd6984f9c21b628629c882eead80
Antonio Ezequiel <antonio.ezequiel@ufpe.br>
2023-03-27 13:53:07 -0300

    ajustes de erros

--- src/main/java/com/movie/battle/moviebattle/classes/Jogo.java
+++ src/main/java/com/movie/battle/moviebattle/classes/Jogo.java
@@ -10,7 +10,7 @@ import com.movie.battle.moviebattle.DTO.UserRankingDTO;
 @Entity
 public class Jogo {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String usuario;
     private int score;
